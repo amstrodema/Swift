bb02776d82d111ae5f1b87b9b90c3fca5a210eb9
DESKTOP-5AP204E\ZYNXX <oluyinka.akinatyo@gmail.com>
2021-05-05 15:00:39 +0100

    Initial commit

---
+++ app/src/main/java/com/example/swift/SmartLogin.java
@@ -0,0 +1,14 @@
+package com.example.swift;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SmartLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.smart_login);
+    }
+}
---
+++ app/src/main/java/com/example/swift/SplashScreen.java
@@ -0,0 +1,14 @@
+package com.example.swift;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+    }
+}
